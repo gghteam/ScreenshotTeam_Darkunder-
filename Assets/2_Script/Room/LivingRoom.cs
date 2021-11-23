e22438d7cadcf693e4e1ce7473ba6e2d57a0f3dc
DESKTOP-OHD6DA9\USER <ggm.s210009@ggh.goe.go.kr>
2021-11-24 01:52:37 +0900

    첫번째 서랍완료

--- Assets/2_Script/Room/LivingRoom.cs
+++ Assets/2_Script/Room/LivingRoom.cs
@@ -5,47 +5,81 @@ using UnityEngine.UI;
 public class LivingRoom : MonoBehaviour
 {
     private int carfetNum = 0;
+    private int drawers1Num = 0;
+    private int drawers2Num = 0;
     [SerializeField]
-    GameObject drawer;
+    GameObject drawers; //서랍장 전체
+
     [SerializeField]
-    private GameObject carfet;
+    private GameObject carfet; //카펫 오브젝트 
+
     [SerializeField]
-    private Button carfetButton;
+    private GameObject drawer1; //첫번째 서랍
+
     [SerializeField]
-    private Button drawerButton1;
+    private Button carfetButton;//카펫 버튼
+
     [SerializeField]
-    private Button drawerExitButton1;
+    private Button drawersButton1;//첫번째 서랍 버튼
+
     [SerializeField]
-    private Button drawerExitButton2;
+    private Button drawerButton1Image2;//확대 되었을때 첫번째 서랍 눌렀을 때 버튼
+
     [SerializeField]
-    private Sprite[] carfetSprites;
+    private Button drawersExitButton1;//서랍장 나가기 버튼
+
+    [SerializeField]
+    private Button drawersExitButton2;//서랍장 나가기 버튼2
+
+    [SerializeField]
+    private Button kitchenButton;//부엌 가기 버튼
+
+    [SerializeField]
+    private Button drawer1button;//확대 되었을때첫번째 서랍 버튼
+
+    [SerializeField]
+    private Sprite[] carfetSprites;//카펫 스프라이트들
+
+    [SerializeField]
+    private Sprite[] drawersSprites;//서랍 스프라이트들
+
     private Image carfetImage;
-    public bool onoff = false;
+
+    private Image drawer1Image;
+
+    public bool onOff = false;
+    public bool objectOnOff = false;
+
     private void Start()
     {
+        drawer1Image = drawer1.GetComponent<Image>();
         carfetImage = carfet.GetComponent<Image>();
-        drawerExitButton1.gameObject.SetActive(false);
-        drawer.SetActive(false);
+        drawersExitButton1.gameObject.SetActive(false);
+        drawersExitButton2.gameObject.SetActive(false);
+        drawerButton1Image2.gameObject.SetActive(false);
+        drawers.SetActive(false);
     }
-    public void DrawerClick()
+    public void DrawersClick()
     {
-        if (onoff == false)
+        if (onOff == false)
         {
-            drawer.SetActive(true);
-            onoff = true;
+            drawers.SetActive(true);
+            onOff = true;
+            kitchenButton.gameObject.SetActive(false);
             carfetButton.gameObject.SetActive(false);
-            drawerExitButton1.gameObject.SetActive(true);
-            drawerExitButton2.gameObject.SetActive(true);
-            drawerButton1.gameObject.SetActive(false);
+            drawersExitButton1.gameObject.SetActive(true);
+            drawersExitButton2.gameObject.SetActive(true);
+            drawersButton1.gameObject.SetActive(false);
         }
-        else if (onoff == true)
+        else if (onOff == true)
         {
-            drawer.SetActive(false);
-            onoff = false;
+            kitchenButton.gameObject.SetActive(true);
+            drawers.SetActive(false);
+            onOff = false;
             carfetButton.gameObject.SetActive(true);
-            drawerExitButton1.gameObject.SetActive(false);
-            drawerExitButton2.gameObject.SetActive(false);
-            drawerButton1.gameObject.SetActive(true);
+            drawersExitButton1.gameObject.SetActive(false);
+            drawersExitButton2.gameObject.SetActive(false);
+            drawersButton1.gameObject.SetActive(true);
         }
     }
     public void CarfetClick()
@@ -58,6 +92,36 @@ public class LivingRoom : MonoBehaviour
         else if (carfetNum == 1)
         {
             --carfetNum;
+            carfetImage.sprite = carfetSprites[carfetNum];
+        }
+    }
+    public void Drawer1Click()
+    {
+        if (drawers1Num == 0)
+        {
+            ++drawers1Num;
+            drawer1Image.sprite = drawersSprites[drawers1Num];
+            drawer1button.gameObject.SetActive(false);
+            drawerButton1Image2.gameObject.SetActive(true);
+        }
+        else if (drawers1Num == 1)
+        {
+            --drawers1Num;
+            drawer1Image.sprite = drawersSprites[drawers1Num];
+            drawer1button.gameObject.SetActive(true);
+            drawerButton1Image2.gameObject.SetActive(false);
+        }
+    }
+    public void Drawer2Click()
+    {
+        if (drawers2Num == 0)
+        {
+            ++drawers2Num;
+            carfetImage.sprite = carfetSprites[carfetNum];
+        }
+        else if (drawers2Num == 1)
+        {
+            --drawers2Num;
             carfetImage.sprite = carfetSprites[carfetNum];
         }
     }
