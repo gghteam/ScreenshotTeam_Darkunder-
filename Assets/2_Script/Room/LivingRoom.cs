13fd507e0d9633f2c650582906170be60297ca6c
DESKTOP-OHD6DA9\USER <ggm.s210009@ggh.goe.go.kr>
2021-11-23 10:08:34 +0900

    no message

--- Assets/2_Script/Room/LivingRoom.cs
+++ Assets/2_Script/Room/LivingRoom.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class livingRoom : MonoBehaviour
+using UnityEngine.UI;
+public class LivingRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int carfetNum = 0;
+    [SerializeField]
+    GameObject drawer;
+    [SerializeField]
+    private GameObject carfet;
+    [SerializeField]
+    private Button carfetButton;
+    [SerializeField]
+    private Button drawerButton1;
+    [SerializeField]
+    private Button drawerExitButton1;
+    [SerializeField]
+    private Button drawerExitButton2;
+    [SerializeField]
+    private Sprite[] carfetSprites;
+    private SpriteRenderer carfetSpriteRenderer;
+    public bool onoff = false;
+    private void Start()
     {
-        
+        carfetSpriteRenderer = carfet.GetComponent<SpriteRenderer>();
+        drawerExitButton1.gameObject.SetActive(false);
+        drawer.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void DrawerClick()
     {
-        
+        if (onoff == false)
+        {
+            drawer.SetActive(true);
+            onoff = true;
+            carfetButton.gameObject.SetActive(false);
+            drawerExitButton1.gameObject.SetActive(true);
+            drawerExitButton2.gameObject.SetActive(true);
+            drawerButton1.gameObject.SetActive(false);
+        }
+        else if (onoff == true)
+        {
+            drawer.SetActive(false);
+            onoff = false;
+            carfetButton.gameObject.SetActive(true);
+            drawerExitButton1.gameObject.SetActive(false);
+            drawerExitButton2.gameObject.SetActive(false);
+            drawerButton1.gameObject.SetActive(true);
+        }
+    }
+    public void CarfetClick()
+    {
+        if (carfetNum == 0)
+        {
+            ++carfetNum;
+            carfetSpriteRenderer.sprite = carfetSprites[carfetNum];
+        }
+        else if (carfetNum == 1)
+        {
+            --carfetNum;
+            carfetSpriteRenderer.sprite = carfetSprites[carfetNum];
+        }
     }
 }
